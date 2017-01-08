83301bbf3cabe2e4832186258265d0090e6b8267
jfriend00 <github@the-friend-family.com>
2017-01-07 17:49:59 -0800

    Move SpecialSet to new file

---
+++ specialset.js
@@ -0,0 +1,130 @@
+class SpecialSet extends Set {
+    constructor(arg) {
+        super(arg);
+    }
+   
+    addTo(iterable) {
+        iterable.forEach(item => {
+            this.add(item);
+        });
+    }
+    
+    equals(otherSet) {
+        if (otherSet.size !== this.size) return false;
+        let equals = true;
+        otherSet.forEach(item => {
+            if (!this.has(item)) {
+                equals = false;
+            }
+        });
+        return equals;
+    }
+    
+    // Get first value.  Since sets are unordered, this is useful only for getting the ONLY value in the set
+    getFirst() {
+        return this.values().next().value;
+    }
+    
+    toArray() {
+        return Array.from(this);
+    }
+    
+    toSortedArrayNumber() {
+        return this.toArray().sort((a,b) => a - b);
+    }
+    
+    toNumberString() {
+        return this.toSortedArrayNumber().join(",");
+    }
+    
+    // remove items in this set that are in the otherIterable
+    // returns a count of number of items removed
+    remove(otherIterable) {
+        let cnt = 0;
+        otherIterable.forEach(item => {
+            if (this.delete(item)) {
+                ++cnt;
+            }
+        });
+        return cnt;
+    }
+    
+    // remove the items from this set that are not in the other iterable
+    // return the  number of items removed
+    removeNonMatching(iterable) {
+        let cnt = 0;
+        // if iterable has a "has" property, then we can just use it directly
+        let other = iterable.hasOwnProperty("has") ? iterable : new SpecialSet(iterable);
+        this.forEach(item => {
+            if (!other.has(item)) {
+                this.delete(item);
+                ++cnt;
+            }
+        });
+        return cnt;
+    }
+    
+    // pass callback function that returns true to keep, false to remove
+    // The callback function is passed the element to be tested
+    removeCustom(fn) {
+        let cnt = 0;
+        this.forEach(item => {
+            if (fn(item) === false) {
+                this.delete(item);
+                cnt++;
+            }
+        });
+        return cnt;
+    }
+    
+    // returns boolean whether every element in this set is in otherSet
+    isSubsetOf(otherSet) {
+        for (let key of this) {
+            if (!otherSet.has(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    // returns a new set of keys that are in this set, but not in otherSet
+    difference(otherSet) {
+        let newSet = new SpecialSet();
+        for (let key of this) {
+            if (!otherSet.has(key)) {
+                newSet.add(key);
+            }
+        }
+        return newSet;
+    }
+    
+    // return union of two or more sets
+    union(...otherSets) {
+        let newSet = new SpecialSet(this);
+        otherSets.forEach(s => {
+            for (let key of s) {
+                newSet.add(key);
+            }
+        });
+        return newSet;
+    }
+    
+    // returns a new set that contains keys that in both sets
+    intersection(iterable) {
+        let newSet = new SpecialSet();
+        iterable.forEach(key => {
+            if (this.has(key)) {
+                newSet.add(key);
+            }
+        })
+        return newSet;
+    }    
+    
+    // return a copy of the current set
+    clone() {
+        return new SpecialSet(this);
+    }
+}    
+
+
+module.exports = SpecialSet;
